0d1b4885a62c473ca47d082bcd778d8b8fc86981
Nnwww <johndororo@gmail.com>
2017-09-22 02:35:49 +0900

    simplify word count only

--- src/WordEmbedding/HasText/Internal/Strict/Model.hs
+++ src/WordEmbedding/HasText/Internal/Strict/Model.hs
@@ -9,13 +9,12 @@ import qualified Data.Text                                        as T
 import qualified Data.IntMap                                   as IntMap
 import qualified Data.HashMap.Strict                              as HS
 import           WordEmbedding.HasText.Internal.Type
-                 ( SpVector
-                 , Weights(..)
+                 ( Weights(..)
                  , WordVecRef
                  )
 
-computeHidden :: WordVecRef -> [T.Text] -> IO (SpVector)
+computeHidden :: WordVecRef -> [T.Text] -> IO (Weights)
 computeHidden wsRef input = do
   ws <- readMVar wsRef
-  pure . IntMap.unionsWith (+) . map (_wI) . map (ws HS.!) $ input
+  pure . Weights . IntMap.unionsWith (+) . map (_wI) . map (ws HS.!) $ input
 {-# INLINE computeHidden #-}
--- src/WordEmbedding/HasText/Model.hs
+++ src/WordEmbedding/HasText/Model.hs
@@ -5,7 +5,6 @@ module WordEmbedding.HasText.Model
   , WordVec
   , WordVecRef
   , Weights(..)
-  , MWeights(..)
   , initLParams
   , updateModel
   , genNoiseDistribution
@@ -18,6 +17,8 @@ import           Control.Monad
 import           Control.Monad.Reader
 import           Data.Binary                                      (Binary)
 import           Data.Binary.Orphans                              ()
+import           Data.IntMap                     (IntMap)
+import qualified Data.IntMap                     as IntMap
 import           Data.Foldable
 import qualified Data.HashMap.Strict                              as HS
 import qualified Data.List                                        as L
@@ -33,7 +34,6 @@ import           WordEmbedding.HasText.Internal.Strict.Model
 import qualified WordEmbedding.HasText.Internal.Strict.MVectorOps as HMV
 import           WordEmbedding.HasText.Internal.Type              (HasTextArgs (..),
                                                                    LParams (..),
-                                                                   MWeights (..),
                                                                    Model,
                                                                    Params (..),
                                                                    Weights (..),
@@ -42,37 +42,16 @@ import           WordEmbedding.HasText.Internal.Type              (HasTextArgs (
 
 instance Binary Weights
 
-initLParams :: Double -> Int -> RM.GenIO -> IO LParams
-initLParams initLR dim gR = LParams
-  <$> newRef (0 :: Double)
-  <*> pure initLR
-  <*> VUM.replicate dim 0.0
-  <*> VUM.replicate dim 0.0
-  <*> pure gR
-
--- |
--- Negative-sampling function, one of the word2vec's efficiency optimization tricks.
-negativeSampling :: T.Text -- ^ a updating target word
-                 -> Model
-negativeSampling input = do
-  (Params{_args = HasTextArgs{_negatives = negs}, _noiseDist = nDist}, LParams{_rand = rand}) <- ask
-  negModels <- liftIO . replicateM (fromIntegral negs - 1) $ sampleNegative nDist rand
-  asum negModels *> samplePositive
-  where
-    samplePositive = binaryLogistic True input
-    sampleNegative noise rand = binaryLogistic False . _eWord <$> getNegative noise rand input
+initLParams :: RM.GenIO -> LParams
+initLParams = LParams 0 IntMap.empty
 
 -- |
 -- The function that update a model. This function is a entry point of LParams module.
-updateModel :: V.Vector T.Text -> T.Text -> Model
+updateModel :: [T.Text] -> T.Text -> Model
 updateModel inputs updTarget = do
-  (Params{_wordVecRef = wvRef}, LParams{_hidden = h, _grad = g}) <- ask
-  liftIO $ computeHidden h wvRef inputs
-  negativeSampling updTarget
-  liftIO . modifyMVar_ wvRef $ \ws -> updateWordVecs g ws *> pure ws
-  where
-    updateWordVecs grad ws = V.mapM_ (addGradmWI grad ws) inputs
-    addGradmWI grad ws k = getmWI ws k `HMV.addMM` grad
+  (Params{_wordVecRef = wvRef}, LParams{_hidden = h}) <- ask
+  hidden <- liftIO $ computeHidden wvRef inputs
+  liftIO $ modifyMVar_ wvRef (pure . HS.insert updTarget hidden)
 
 getNegative :: RMC.CondensedTableV Entry -> RM.GenIO -> T.Text -> IO Entry
 getNegative noiseTable rand input = tryLoop

d919265540c451484f3805ec25716d2725a4f386
Nnwww <johndororo@gmail.com>
2017-10-10 14:49:43 +0900

    discard nix

--- default.nix
+++
@@ -1,9 +0,0 @@
-{nixpkgs ? import <nixpkgs> { }, ghc ? nixpkgs.ghc}:
-
-with nixpkgs;
-
-haskell.lib.buildStackProject {
-  name = "wcVector";
-  buildInputs = [ zlib.dev zlib.out pkgconfig ncurses git ];
-  ghc = haskell.compiler.ghc802;
- }
